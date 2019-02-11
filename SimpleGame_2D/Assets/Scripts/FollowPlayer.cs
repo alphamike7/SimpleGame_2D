61200a811c9c8437c000ab38a1fa5f083c850e6f
Samson M Mathews <samsonmmathews@gmail.com>
2019-02-11 15:20:06 +0530

    Added a player and few ground objects

---
+++ SimpleGame_2D/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform player;
+    public Vector2 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position;
+    }
+}
---
+++ SimpleGame_2D/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+    //public float movespeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(4, rb.velocity.y);
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if(Input.GetMouseButtonDown(0) && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+       // if (Input.GetKey(KeyCode.LeftArrow))
+       // {
+       //     rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+       // }
+       // if (Input.GetKey(KeyCode.RightArrow))
+      //  {
+      //      rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+       // }
+    }
+}
