134561cd1d79765146dbd332775b810fa8f42796
Samson M Mathews <samsonmmathews@gmail.com>
2019-02-11 16:37:56 +0530

    made the game for android

---
+++ SimpleGame_2D/Assets/Scripts/Controls.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movespeed;
+    public float upspeed;
+    public bool moveright;
+    public bool moveleft;
+    public bool moveup;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, upspeed);
+
+        }
+
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, upspeed);
+        }
+
+        if (moveright)
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+    }
+}
---
+++ SimpleGame_2D/Assets/Scripts/Touch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour
+{
+    private Controls player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<Controls>();
+    }
+
+    public void LeftArrow()
+    {
+        player.moveright = false;
+        player.moveleft = true;
+        player.moveup = false;
+    }
+    public void RightArrow()
+    {
+        player.moveright = true;
+        player.moveleft = false;
+        player.moveup = false;
+    }
+    public void UpArrow()
+    {
+        player.moveright = false;
+        player.moveleft = false;
+        player.moveup = true;
+    }
+    public void ReleaseLeftArrow()
+    {
+        player.moveleft = false;
+    }
+    public void ReleaseRightArrow()
+    {
+        player.moveright = false;
+
+    }
+    public void ReleaseUpArrow()
+    {
+        player.moveup = false;
+
+    }
+
+}
